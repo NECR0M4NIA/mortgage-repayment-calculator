ebe410c1a52032a54f80de46d81b17196430ec3d
NECR0M4NIA <gary.necromania@gmail.com>
2025-03-13 14:53:02 +0100

    Update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
+// RESULTS CONTAINER
+const $finalResults = document.querySelector('.final-results')
+
 // BTN
 const $clearBtn = document.querySelector('#clear-btn')
+const $calculateBtn = document.querySelector('.calculate-btn')
+//
 
 // INPUTS
 const $amountInput = document.querySelector('#amount')
 const $yearsInput = document.querySelector('#years')
 const $pourcentaInput = document.querySelector('#pourcenta')
 const $interestInput = document.querySelector('#interest')
+//
 
-console.log($yearsInput)
+// OTHERS
+const $errorMsg = document.querySelectorAll('.error')
 
+console.log($finalResults)
+
+// FOCUS
+$amountInput.focus()
+//
+
+const inputs = [$amountInput, $interestInput, $pourcentaInput, $yearsInput]
+
+// Fonction bouton calculer
+$calculateBtn.addEventListener('click', e => {
+    $errorMsg.forEach(e => {
+        if (inputs.values === null) {
+            e.classList.remove('hidden')
+        } else {
+            e.classList.add('hidden')
+        }
+    })
+})
+
+// Nettoyer les champs
 $clearBtn.addEventListener('click', clear => {
-    $amountInput.textContent = ""
-    $yearsInput.textContent = ""
-    $pourcentaInput.textContent = ""
-    $interestInput.textContent = ""
+    inputs.forEach(e => {
+        e.value = null
+    })
 })
